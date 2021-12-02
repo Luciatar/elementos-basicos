500e6e0596e679b74d4198f08397c87ca1aa362b
Luciatar <braenlun@gmail.com>
2021-12-02 14:01:36 +0100

    added clientjs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-//create a function that will validate a input number is a DNI
+const formEle= document.querySelector("#crud__user-form")
+const submitButt= document.querySelector(".crud__store-button")
+let client = new ClientJS();
+let fingerprint = client.getFingerprint(); // Get Client's Fingerprint
+submitButt.addEventListener("click", function(e) {
+    submitForm()
+    e.preventDefault()
+})
+
+
+function submitForm() {
+    const formData= new FormData(formEle)
+    formData.append("finguerprint", fingerprint)
+    for (var pair of formData.entries()) {
+        console.log(pair[0]+ ', ' + pair[1]); 
+    }
+}
+
