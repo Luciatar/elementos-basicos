c2d78707bf9a107939f8e23fbe579b129e333457
Luciatar <braenlun@gmail.com>
2021-12-07 15:50:40 +0100

    added ck editor

--- js/main.js
+++ js/main.js
@@ -2,8 +2,17 @@
 import {logoHandler} from "./logo.js";
 import {tabHandler} from "./tabHandler.js";
 import {formHandler} from "./formHandler.js";
+import '../node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js'
 
 logoHandler();
 tabHandler();
-formHandler();
-CKEDITOR.replace( 'editor1' );
+formHandler(); 
+
+ClassicEditor
+    .create( document.querySelector( '#editor1' ) )
+    .then( editor => {
+        console.log( editor );
+    } )
+    .catch( error => {
+        console.error( error );
+    } );
