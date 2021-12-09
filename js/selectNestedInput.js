87b5c25206b1abfade0aae25d29d9c97d65c2f8b
Luciatar <braenlun@gmail.com>
2021-12-09 15:29:23 +0100

    nested inputs now disable if hidden

--- js/selectNestedInput.js
+++ js/selectNestedInput.js
@@ -2,16 +2,41 @@ import { fadeIn, fadeOut } from "./fadeInFadeOut.js";
 const selectElements = document.querySelectorAll(".select-nested-input")
 const nestedInputs = [...document.querySelectorAll(".select-nested-input__option")]
 
+const disableAllChildren = (element) => {
+    let children = [... element.children]
+    
+    children.forEach(ele => {
+        if (ele.tagName == "INPUT" || ele.tagName == "SELECT" || ele.tagName == "TEXTAREA") {
+             ele.setAttribute('disabled', true)
+        }
+    })
+}
+const enableAllChildren = (element) => {
+    let children = [... element.children]
+
+    children.forEach(ele => {
+        if (ele.tagName == "INPUT" || ele.tagName == "SELECT" || ele.tagName == "TEXTAREA") {
+             ele.removeAttribute('disabled')
+        }
+    })
+}
+
 export const selectNestedInputHandlder = () => {
     selectElements.forEach(select => {
         let selectOptions = nestedInputs.filter(input => input.dataset.selectName === select.name);
         select.addEventListener("change", function () {
             selectOptions.forEach(optionElement => {
+               
                 fadeOut(optionElement)
+                disableAllChildren(optionElement)
                 if (optionElement.dataset.option === select.value) {
+                    enableAllChildren(optionElement)
                     fadeIn(optionElement)
+                
                 }
             })
         })
     });
 }
+
+
